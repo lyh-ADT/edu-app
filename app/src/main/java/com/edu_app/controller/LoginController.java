4e54a69408a47391c2ea7b7313abdf4b3418a2cf
lyh-ADT <1043970238@qq.com>
2020-02-01 01:10:13 +0800

    /实现 基础的登录逻辑(登录失败的提示信息)

--- app/src/main/java/com/edu_app/controller/LoginController.java
+++ app/src/main/java/com/edu_app/controller/LoginController.java
@@ -1,8 +1,10 @@
 package com.edu_app.controller;
 
+import android.os.Looper;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.fragment.app.Fragment;
 import androidx.fragment.app.FragmentManager;
@@ -13,19 +15,22 @@ import com.edu_app.model.Login;
 import com.edu_app.view.LoginAndRegisterFragment;
 
 public class LoginController {
+    private Fragment fragment;
     public LoginController(Fragment fragment, View view){
-        bindListener(fragment, view);
+        this.fragment = fragment;
+        bindListener(view);
     }
 
-    private void bindListener(final Fragment fragment,final View view){
+    private void bindListener(final View view){
         Button login_btn = view.findViewById(R.id.login_btn);
         login_btn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 TextView userName_tv = view.findViewById(R.id.username_edit);
                 TextView userPassword_tv = view.findViewById(R.id.password_edit);
-                // TODO: 添加具体的登录逻辑
-                Login.sendLoginRequest(userName_tv.getText().toString(), userPassword_tv.getText().toString());
+
+                Login login = new Login(LoginController.this);
+                login.sendRequest(userName_tv.getText().toString(), userPassword_tv.getText().toString());
             }
         });
 
@@ -38,5 +43,16 @@ public class LoginController {
                 transaction.replace(R.id.login_activity, LoginAndRegisterFragment.newInstance("register")).commit();
             }
         });
+    }
+
+    public void loginSuccess(String uid){
+        // TODO：添加具体的登录成功后的逻辑
+        fragment.requireActivity().finish();
+    }
+
+    public void loginFail(String reason){
+        Looper.prepare();
+        Toast.makeText(fragment.getContext(), reason, Toast.LENGTH_LONG).show();
+        Looper.loop();
     }
 }
--- app/src/main/java/com/edu_app/model/Login.java
+++ app/src/main/java/com/edu_app/model/Login.java
@@ -1,8 +1,80 @@
 package com.edu_app.model;
 
-public class Login {
-    // TODO: 具体实现
-    static public void sendLoginRequest(String userName, String password){
+import com.edu_app.controller.LoginController;
 
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
+
+public class Login {
+    private LoginController controller;
+
+    public Login(LoginController controller){
+        this.controller = controller;
+    }
+
+    public void sendRequest(final String userName, final String password){
+        new Thread(){
+            @Override
+            public void run(){
+                post(userName, password);
+            }
+        }.start();
+    }
+
+    private void post(String userName, String password){
+        final String SERVER_HOST = "http://192.168.123.22:2000";// TODO: 修改为服务器地址
+        URL url;
+        try {
+            url = new URL(SERVER_HOST);
+        } catch (MalformedURLException e) {
+            e.printStackTrace();
+            return;
+        }
+
+        try {
+            HttpURLConnection connection = (HttpURLConnection)url.openConnection();
+            String data = "user="+userName+"&password="+password;
+
+            connection.setRequestMethod("POST");
+            connection.setDoInput(true);
+            connection.setDoOutput(true);
+            connection.setUseCaches(false);
+
+            connection.setRequestProperty("Content-Length", String.valueOf(data.length()));
+
+            connection.connect();
+
+            OutputStream outputStream = connection.getOutputStream();
+
+            outputStream.write(data.getBytes());
+
+            outputStream.close();
+
+            InputStream inputStream = connection.getInputStream();
+            StringBuilder responseBuilder = new StringBuilder();
+            byte[] buffer = new byte[1024];
+            int len;
+            while((len = inputStream.read(buffer, 0, buffer.length)) != -1){
+                responseBuilder.append(new String(buffer, 0, len));
+            }
+
+            int responseCode = connection.getResponseCode();
+            String response = responseBuilder.toString();
+            if(responseCode == 200){
+                controller.loginSuccess(response);
+            }else if(responseCode == 201){
+                controller.loginFail(response);
+            }
+
+            connection.disconnect();
+
+        } catch (IOException e) {
+            e.printStackTrace();
+            controller.loginFail("网络连接超时");
+        }
     }
 }

5ab0644f3cf280216636a270fee63deb9f323ac2
lyh-ADT <1043970238@qq.com>
2020-02-01 13:48:58 +0800

    实现 基础的注册逻辑(注册失败的提示信息，成功后跳转回登录)

--- app/src/main/java/com/edu_app/controller/RegisterController.java
+++ app/src/main/java/com/edu_app/controller/RegisterController.java
@@ -1,23 +1,27 @@
 package com.edu_app.controller;
 
+import android.os.Looper;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.fragment.app.Fragment;
 import androidx.fragment.app.FragmentManager;
 import androidx.fragment.app.FragmentTransaction;
 
 import com.edu_app.R;
-import com.edu_app.model.Login;
+import com.edu_app.model.Register;
 import com.edu_app.view.LoginAndRegisterFragment;
 
 public class RegisterController {
+    private Fragment fragment;
     public RegisterController(Fragment fragment, View view){
-        bindListener(fragment, view);
+        this.fragment = fragment;
+        bindListener(view);
     }
 
-    private void bindListener(final Fragment fragment, final View view){
+    private void bindListener(final View view){
         Button login_btn = view.findViewById(R.id.back_to_login_btn);
         login_btn.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -38,11 +42,30 @@ public class RegisterController {
 
                 String userPassword = userPassword_tv.getText().toString();
                 String userRepeatPassword = userRepeatPassword_tv.getText().toString();
-                // TODO: 添加具体的注册逻辑
+
                 if(userPassword.equals(userRepeatPassword)){
-                    Login.sendLoginRequest(userName_tv.getText().toString(), userPassword);
+                    Register register = new Register(RegisterController.this);
+                    register.sendRequest(userName_tv.getText().toString(), userPassword);
+                }else{
+                    Toast.makeText(view.getContext(), "两次密码不相同", Toast.LENGTH_LONG).show();
                 }
             }
         });
+    }
+
+    public void registerSuccess(){
+        Looper.prepare();
+        Toast.makeText(fragment.getContext(), "注册成功", Toast.LENGTH_LONG).show();
+        // 跳转回登录界面让用户登录
+        FragmentManager fragmentManager = fragment.requireFragmentManager();
+        FragmentTransaction transaction = fragmentManager.beginTransaction();
+        transaction.replace(R.id.login_activity, LoginAndRegisterFragment.newInstance("login")).commit();
+        Looper.loop();
+    }
+
+    public void registerFail(String reason){
+        Looper.prepare();
+        Toast.makeText(fragment.getContext(), reason, Toast.LENGTH_LONG).show();
+        Looper.loop();
     }
 }

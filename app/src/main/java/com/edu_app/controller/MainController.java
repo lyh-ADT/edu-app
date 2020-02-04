525677ad164802510295c3875992c6552f541dab
MrZheng99 <masterzheng1125@gmail.com>
2020-02-04 14:17:50 +0800

    #调整整体文件结构 #实现个人页面与其中子选项页面之间的跳转，即点击个人信息跳转至个人信息页面，按返回，则返回个人页面

---
+++ app/src/main/java/com/edu_app/controller/MainController.java
@@ -0,0 +1,51 @@
+package com.edu_app.controller;
+
+
+import android.content.Context;
+import android.content.SharedPreferences;
+import android.os.Bundle;
+
+import java.io.File;
+
+public class MainController {
+    private Context context;
+    public String uid;
+    public MainController(Context context){
+        this.context=context;
+    }
+    public Boolean existUidFile(){
+//        判断本地是否有这个文件
+        File file = new File("uid");
+        if (file.exists()) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    public boolean uidIsRight(){
+//        判断该文件是否存在
+        if(existUidFile()){
+            SharedPreferences pref = context.getSharedPreferences("uid",context.MODE_PRIVATE);
+            String uid = pref.getString("uid","");
+            this.uid=uid;
+
+//        TODO:    判断该uid是否有效，如果有效就携带uid跳转到个人功能界面
+            //        if(uid过期){
+//            return false;
+//        }
+            return true;
+        }else{
+            return false;
+        }
+    }
+    public Bundle getUidBundle(){
+        if(uidIsRight()){
+            Bundle bundle = new Bundle();
+            bundle.putCharSequence("uid",this.uid);
+            return bundle;
+        }
+        return null;
+    }
+}
+
---
+++ app/src/main/java/com/edu_app/model/student/StudentInfo.java
@@ -0,0 +1,6 @@
+package com.edu_app.model.student;
+
+import com.edu_app.model.UserInfo;
+
+public class StudentInfo extends UserInfo {
+}

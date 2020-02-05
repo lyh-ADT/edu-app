6f41ad6556928d97224032cba4a6e2e91ff8cb36
lyh-ADT <1043970238@qq.com>
2020-02-05 13:39:39 +0800

    添加  activity_function底部的导航栏id(navigation_bar)  教师直播页面设置全屏的方法

--- app/src/main/java/com/edu_app/controller/teacher/stream/StreamPageController.java
+++ app/src/main/java/com/edu_app/controller/teacher/stream/StreamPageController.java
@@ -6,6 +6,7 @@ import android.view.View;
 
 import android.app.Fragment;
 
+import com.edu_app.R;
 import com.edu_app.controller.teacher.Controller;
 import com.edu_app.model.teacher.TeacherInfo;
 import com.edu_app.model.teacher.stream.LiveStreamPage;
@@ -18,5 +19,18 @@ public class StreamPageController extends Controller {
         super(view, new LiveStreamPage());
         this.fragment = fragment;
         this.info = info;
+        setFullScreen();
+    }
+
+    private void setFullScreen(){
+        Activity activity = fragment.getActivity();
+        // 关闭标题栏
+        ActionBar actionBar = activity.getActionBar();
+        if(actionBar != null){
+            actionBar.hide();
+        }
+        // 关闭底部的导航栏
+        View navigation_bar = activity.findViewById(R.id.navigation_bar);
+        navigation_bar.setVisibility(View.GONE);
     }
 }

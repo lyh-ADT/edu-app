7bda60a5e0d584747691aafe8bbde8cdf2a1261b
lyh-ADT <1043970238@qq.com>
2020-02-11 19:32:26 +0800

    添加 注册界面需要填写的信息

--- app/src/main/java/com/edu_app/model/login/Register.java
+++ app/src/main/java/com/edu_app/model/login/Register.java
@@ -1,11 +1,9 @@
 package com.edu_app.model.login;
 
 import com.edu_app.controller.login.RegisterController;
+import com.edu_app.model.NetworkUtility;
 
 import java.io.IOException;
-import java.io.InputStream;
-import java.io.OutputStream;
-import java.net.HttpURLConnection;
 import java.net.MalformedURLException;
 import java.net.URL;
 
@@ -16,65 +14,36 @@ public class Register {
         this.controller = controller;
     }
 
-    public void sendRequest(final String userName, final String password) {
+
+    public void post(final Form form){
+        final String Host = "http://192.168.123.22:2000";
         new Thread(){
             @Override
             public void run(){
-                post(userName, password);
+                try {
+                    String response = NetworkUtility.postRequest(Host+"/register", "", form);
+                    if("success".equals(response)){
+                        controller.registerSuccess();
+                    }else{
+                        controller.registerFail(response);
+                    }
+                } catch (IOException e) {
+                    e.printStackTrace();
+                    controller.registerFail("网络连接超时");
+                }
             }
         }.start();
     }
 
-    private void post(String userName, String password){
-        final String SERVER_HOST = "http://192.168.123.22:2000/register";// TODO: 修改为服务器地址
-        URL url;
-        try {
-            url = new URL(SERVER_HOST);
-        } catch (MalformedURLException e) {
-            e.printStackTrace();
-            return;
-        }
-
-        try {
-            HttpURLConnection connection = (HttpURLConnection)url.openConnection();
-            String data = "user="+userName+"&password="+password;
-
-            connection.setRequestMethod("POST");
-            connection.setDoInput(true);
-            connection.setDoOutput(true);
-            connection.setUseCaches(false);
-
-            connection.setRequestProperty("Content-Length", String.valueOf(data.length()));
-
-            connection.connect();
-
-            OutputStream outputStream = connection.getOutputStream();
-
-            outputStream.write(data.getBytes());
-
-            outputStream.close();
-
-            InputStream inputStream = connection.getInputStream();
-            StringBuilder responseBuilder = new StringBuilder();
-            byte[] buffer = new byte[1024];
-            int len;
-            while((len = inputStream.read(buffer, 0, buffer.length)) != -1){
-                responseBuilder.append(new String(buffer, 0, len));
-            }
-
-            int responseCode = connection.getResponseCode();
-            String response = responseBuilder.toString();
-            if(responseCode == 200){
-                controller.registerSuccess();
-            }else if(responseCode == 201){
-                controller.registerFail(response);
-            }
-
-            connection.disconnect();
-
-        } catch (IOException e) {
-            e.printStackTrace();
-            controller.registerFail("网络连接超时");
-        }
+    public static class Form{
+        public String userId;
+        public String userName;
+        public String sex;
+        public String age;
+        public String birthday;
+        public String phone;
+        public String qq;
+        public String address;
+        public String password;
     }
 }

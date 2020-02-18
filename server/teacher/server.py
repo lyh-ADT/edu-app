eed2ac1055a7850b64be854030bc8fc76f33a198
lyh-ADT <1043970238@qq.com>
2020-02-18 12:11:28 +0800

    教师删除练习后端

--- server/teacher/server.py
+++ server/teacher/server.py
@@ -2,11 +2,13 @@ import tornado
 from TeaGetClassListRequestHandler import TeaGetClassListRequestHandler
 from TeaGetPracticeListRequestHandler import TeaGetPracticeListRequestHandler
 from TeaPushPracticeRequestHandler import TeaPushPracticeRequestHandler
+from TeaDeletePracticeRequestHandler import TeaDeletePracticeRequestHandler
 
 app = tornado.web.Application(handlers=[
     (r"/classList", TeaGetClassListRequestHandler),
     (r"/practice", TeaGetPracticeListRequestHandler),
-    (r"/add_practice", TeaPushPracticeRequestHandler)
+    (r"/add_practice", TeaPushPracticeRequestHandler),
+    (r"/delete_practice", TeaDeletePracticeRequestHandler)
 ])
 http_server = tornado.httpserver.HTTPServer(app)
 # http_server.listen(8100)

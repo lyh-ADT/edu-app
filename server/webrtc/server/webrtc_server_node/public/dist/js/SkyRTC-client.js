132402c132176816933d93a0b49912a413fbbcf7
lyh-ADT <1043970238@qq.com>
2020-03-04 15:17:21 +0800

    修改教师直播平台web端的iceServers和安卓端一样

--- server/webrtc/server/webrtc_server_node/public/dist/js/SkyRTC-client.js
+++ server/webrtc/server/webrtc_server_node/public/dist/js/SkyRTC-client.js
@@ -21,12 +21,17 @@ const SkyRTC = function () {
                 "url": "stun:stun.l.google.com:19302"
             },
             {
-                "url": "stun:global.stun.twilio.com:3478"
+                "url": "stun:47.93.186.97:3478?transport=udp"
             },
             {
-                "url": "turn:global.stun.twilio.com:3478",
-                "username": "79fdd6b3c57147c5cc44944344c69d85624b63ec30624b8674ddc67b145e3f3c",
-                "credential": "xjfTOLkVmDtvFDrDKvpacXU7YofAwPg6P6TXKiztVGw"
+                "url": "turn:47.93.186.97:3478?transport=udp",
+                "username": "ddssingsong",
+                "credential": "123456"
+            },
+            {
+                "url": "turn:47.93.186.97:3478?transport=tcp",
+                "username": "ddssingsong",
+                "credential": "123456"
             }
         ]
     };
